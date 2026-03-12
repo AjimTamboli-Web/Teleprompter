bb5eb6d62db55196e0c16c4915c4db2212f1fa87
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-03-12 09:36:00 +0530

    User Repository created

---
+++ src/main/java/com/teleprompter/teleprompter/UserRepository.java
@@ -0,0 +1,16 @@
+package com.teleprompter.teleprompter;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID>{
+
+	Optional<User> findByEmail(String email);
+	
+	boolean existsByPhone(String phone);
+	
+}
