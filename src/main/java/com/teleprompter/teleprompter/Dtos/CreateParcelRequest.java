13e9620fd8fd170de5c95baaaf01a01340268c6e
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-02 10:56:24 +0530

    used lombok for reduce boilerplate

--- src/main/java/com/teleprompter/teleprompter/Dtos/CreateParcelRequest.java
+++ src/main/java/com/teleprompter/teleprompter/Dtos/CreateParcelRequest.java
@@ -9,7 +9,10 @@ import jakarta.validation.constraints.DecimalMin;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter  @Setter
 public class CreateParcelRequest {
 
 	@NotNull
@@ -40,70 +43,70 @@ public class CreateParcelRequest {
 	
 
 	
-	
-	public Boolean getFragile() {
-		return fragile;
-	}
-
-	public void setFragile(Boolean fragile) {
-		this.fragile = fragile;
-	}
-
-	public UUID getSenderId() {
-		return senderId;
-	}
-
-	public void setSenderId(UUID senderId) {
-		this.senderId = senderId;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public BigDecimal getWeight() {
-		return weight;
-	}
-
-	public void setWeight(BigDecimal weight) {
-		this.weight = weight;
-	}
-
-	public BigDecimal getValue() {
-		return value;
-	}
-
-	public void setValue(BigDecimal value) {
-		this.value = value;
-	}
-
-	public Boolean getRestrictedItemsDeclared() {
-		return restrictedItemsDeclared;
-	}
-
-	public void setRestrictedItemsDeclared(Boolean restrictedItemsDeclared) {
-		this.restrictedItemsDeclared = restrictedItemsDeclared;
-	}
-
-	public String getPhotoUrl() {
-		return photoUrl;
-	}
-
-	public void setPhotoUrl(String photoUrl) {
-		this.photoUrl = photoUrl;
-	}
-
-	public ParcelCategory getCategory() {
-		return category;
-	}
-
-	public void setCategory(ParcelCategory category) {
-		this.category = category;
-	}
-	
+//	
+//	public Boolean getFragile() {
+//		return fragile;
+//	}
+//
+//	public void setFragile(Boolean fragile) {
+//		this.fragile = fragile;
+//	}
+//
+//	public UUID getSenderId() {
+//		return senderId;
+//	}
+//
+//	public void setSenderId(UUID senderId) {
+//		this.senderId = senderId;
+//	}
+//
+//	public String getDescription() {
+//		return description;
+//	}
+//
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
+//
+//	public BigDecimal getWeight() {
+//		return weight;
+//	}
+//
+//	public void setWeight(BigDecimal weight) {
+//		this.weight = weight;
+//	}
+//
+//	public BigDecimal getValue() {
+//		return value;
+//	}
+//
+//	public void setValue(BigDecimal value) {
+//		this.value = value;
+//	}
+//
+//	public Boolean getRestrictedItemsDeclared() {
+//		return restrictedItemsDeclared;
+//	}
+//
+//	public void setRestrictedItemsDeclared(Boolean restrictedItemsDeclared) {
+//		this.restrictedItemsDeclared = restrictedItemsDeclared;
+//	}
+//
+//	public String getPhotoUrl() {
+//		return photoUrl;
+//	}
+//
+//	public void setPhotoUrl(String photoUrl) {
+//		this.photoUrl = photoUrl;
+//	}
+//
+//	public ParcelCategory getCategory() {
+//		return category;
+//	}
+//
+//	public void setCategory(ParcelCategory category) {
+//		this.category = category;
+//	}
+//	
 	
 }
