db8d331d9e951ea37982c94999f43c4a2500009e
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-06 14:53:05 +0530

    implementing validations on class

--- src/main/java/com/teleprompter/teleprompter/Dtos/RegisterUserRequest.java
+++ src/main/java/com/teleprompter/teleprompter/Dtos/RegisterUserRequest.java
@@ -1,6 +1,8 @@
 package com.teleprompter.teleprompter.dtos;
 
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,14 +11,21 @@ import lombok.Setter;
 @Getter @Setter
 public class RegisterUserRequest {
 
-
+	
+	@NotBlank
+	@Size(max = 100)
 	private String name;
 	
+	@NotBlank
+	@Email
 	private String email;
 	
+	@NotBlank
 	private String phone;
 	
-	private Long passwordHash;
+	@NotBlank
+	@Size(min = 8,max = 72)
+	private String password;
 	
 	
 }
