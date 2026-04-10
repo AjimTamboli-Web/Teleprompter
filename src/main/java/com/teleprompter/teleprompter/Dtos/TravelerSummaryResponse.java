bffe62f8c8d98dc862289d202cea1012fedc1a53
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-10 15:02:51 +0530

    creating for journey response

---
+++ src/main/java/com/teleprompter/teleprompter/Dtos/TravelerSummaryResponse.java
@@ -0,0 +1,16 @@
+package com.teleprompter.teleprompter.dtos;
+
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class TravelerSummaryResponse {
+
+	private UUID id;
+	private String name;
+	
+	
+	
+}
