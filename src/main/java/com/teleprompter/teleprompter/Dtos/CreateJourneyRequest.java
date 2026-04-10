d3b7b7375ce2da81b60ef5578b94efcf9f8ca383
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-10 14:38:16 +0530

    Create journey dto for request

--- src/main/java/com/teleprompter/teleprompter/Dtos/CreateJourneyRequest.java
+++ src/main/java/com/teleprompter/teleprompter/Dtos/CreateJourneyRequest.java
@@ -1,5 +1,27 @@
 package com.teleprompter.teleprompter.dtos;
 
+import java.math.BigDecimal;
+
+import com.teleprompter.teleprompter.enums.TransportMode;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter  @Setter
 public class CreateJourneyRequest {
 
+	
+	private Long travelerId;
+	
+	private Long sourceCityId;
+	
+	private Long destinationCityId;
+	
+	private BigDecimal maxWeightCapacity;
+	
+	private TransportMode transportMode;
+	
+	
+	
+	
 }
