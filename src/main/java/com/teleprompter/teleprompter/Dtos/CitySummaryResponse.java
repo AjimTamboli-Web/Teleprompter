13b43b57f5add8448ce130faa74c42219bf01ecf
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-10 15:00:22 +0530

    creating for journey response

---
+++ src/main/java/com/teleprompter/teleprompter/Dtos/CitySummaryResponse.java
@@ -0,0 +1,15 @@
+package com.teleprompter.teleprompter.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter     @Setter
+public class CitySummaryResponse {
+
+	
+	private Long id;
+	private String cityName;
+	private String state;
+	
+	
+}
