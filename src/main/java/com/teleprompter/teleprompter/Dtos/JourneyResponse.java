c23758a7c09ff5b8be8d9981ab476a460c26d3ee
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-10 14:39:53 +0530

    creating Journey's dto for Response

--- src/main/java/com/teleprompter/teleprompter/Dtos/JourneyResponse.java
+++ src/main/java/com/teleprompter/teleprompter/Dtos/JourneyResponse.java
@@ -1,5 +1,38 @@
 package com.teleprompter.teleprompter.dtos;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.teleprompter.teleprompter.entity.City;
+import com.teleprompter.teleprompter.entity.User;
+import com.teleprompter.teleprompter.enums.JourneyStatus;
+import com.teleprompter.teleprompter.enums.TransportMode;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter   @Setter
 public class JourneyResponse {
 
+	private Long id;
+	
+	private User traveler;
+	
+	private City sourceCity;
+	
+	private City destinationCity;
+	
+	private LocalDateTime departureTime;
+	
+	private LocalDateTime estimatedArrivalTime;
+	
+	private BigDecimal maxWeightCapacity;
+	
+	private BigDecimal availableWeightCapacity;
+	
+	private TransportMode transportMode;
+	
+	private JourneyStatus status;
+	
 }
