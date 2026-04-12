a38f2daa56747202f47ed6ce25eb710f49af74d8
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-12 15:41:30 +0530

    implementing validation on this class

--- src/main/java/com/teleprompter/teleprompter/Dtos/CreateJourneyRequest.java
+++ src/main/java/com/teleprompter/teleprompter/Dtos/CreateJourneyRequest.java
@@ -1,24 +1,39 @@
 package com.teleprompter.teleprompter.dtos;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 import com.teleprompter.teleprompter.enums.TransportMode;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter  @Setter
 public class CreateJourneyRequest {
 
+	@NotNull
+	private UUID travelerId;
 	
-	private Long travelerId;
-	
+	@NotNull
 	private Long sourceCityId;
 	
+	@NotNull
 	private Long destinationCityId;
 	
+	@NotNull
+	private LocalDateTime departureTime;
+	
+	@NotNull
+	private LocalDateTime estimatedArrivalTime;
+	
+	@NotNull
+	@DecimalMin(value = "0.1")
 	private BigDecimal maxWeightCapacity;
 	
+	@NotNull
 	private TransportMode transportMode;
 	
 	
