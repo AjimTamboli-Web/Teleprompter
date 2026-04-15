d1c6dd287b76e31b3dbfc430dba9612d42428f0f
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-15 11:00:35 +0530

    implementing validations on it

--- src/main/java/com/teleprompter/teleprompter/Dtos/CreateDeliveryRequest.java
+++ src/main/java/com/teleprompter/teleprompter/Dtos/CreateDeliveryRequest.java
@@ -1,17 +1,26 @@
 package com.teleprompter.teleprompter.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter  @Setter
 public class CreateDeliveryRequest {
-
+ 
+	@NotNull
 	private Long parcelId;
 	
+	@NotNull
 	private Long journeyId;
 	
+	@NotBlank
+	@Size(max = 255)
 	private String pickupAddress;
 	
+	@NotBlank
+	@Size(max = 255)
 	private String dropAddress;
 	
 	
