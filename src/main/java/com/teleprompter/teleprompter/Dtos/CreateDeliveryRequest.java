b9bc54d06447fb03c5132814dc76badd4300a855
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-15 10:56:00 +0530

    creating delivery request dto

--- src/main/java/com/teleprompter/teleprompter/Dtos/CreateDeliveryRequest.java
+++ src/main/java/com/teleprompter/teleprompter/Dtos/CreateDeliveryRequest.java
@@ -1,5 +1,20 @@
 package com.teleprompter.teleprompter.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter  @Setter
 public class CreateDeliveryRequest {
 
+	private Long parcelId;
+	
+	private Long journeyId;
+	
+	private String pickupAddress;
+	
+	private String dropAddress;
+	
+	
+	
+	
 }
