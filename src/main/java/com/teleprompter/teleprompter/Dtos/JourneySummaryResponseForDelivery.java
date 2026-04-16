081be7e6b1326022a64e62f37c14841e4dad9302
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-16 14:49:34 +0530

    journey summary for delivery response dto

--- src/main/java/com/teleprompter/teleprompter/Dtos/JourneySummaryResponseForDelivery.java
+++ src/main/java/com/teleprompter/teleprompter/Dtos/JourneySummaryResponseForDelivery.java
@@ -1,5 +1,27 @@
 package com.teleprompter.teleprompter.dtos;
 
+import java.time.LocalDateTime;
+
+import com.teleprompter.teleprompter.enums.TransportMode;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter   @Setter
 public class JourneySummaryResponseForDelivery {
 
+	
+	private Long id;
+	
+	private LocalDateTime departureTime;
+	private TransportMode transportMode;
+
+	// Reusing CitySummaryResponse legitimately based on shared purpose
+	private CitySummaryResponse sourceCity;
+	private CitySummaryResponse destinationCity;
+	
+	
+	
+	
 }
