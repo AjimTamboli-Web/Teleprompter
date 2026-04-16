b2711c0fe5ee7a730d4a9e3dc915b4e45e75c9eb
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-16 14:51:11 +0530

    parcel summary for delivery request response dto

--- src/main/java/com/teleprompter/teleprompter/Dtos/ParcelSummaryForRequestResponse.java
+++ src/main/java/com/teleprompter/teleprompter/Dtos/ParcelSummaryForRequestResponse.java
@@ -1,5 +1,29 @@
 package com.teleprompter.teleprompter.dtos;
 
+import java.math.BigDecimal;
+
+import com.teleprompter.teleprompter.enums.ParcelCategory;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter @Getter
 public class ParcelSummaryForRequestResponse {
 
+	private Long id;
+	
+	private String description;
+	
+	private BigDecimal weight;
+	
+	private ParcelCategory category;
+	
+	private boolean fragile;
+	
+	private String photoUrl;
+	
+	
+	
+	
 }
