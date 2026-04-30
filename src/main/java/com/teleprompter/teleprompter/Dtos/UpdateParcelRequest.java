fd420abe12f0c56897c25b351176399addfcc921
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-04-30 11:35:19 +0530

    with no validation for Patch method

--- src/main/java/com/teleprompter/teleprompter/Dtos/UpdateParcelRequest.java
+++ src/main/java/com/teleprompter/teleprompter/Dtos/UpdateParcelRequest.java
@@ -1,9 +1,31 @@
 package com.teleprompter.teleprompter.dtos;
 
+import java.math.BigDecimal;
+import com.teleprompter.teleprompter.enums.ParcelCategory;
+import jakarta.validation.constraints.DecimalMin;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter    @Setter
 public class UpdateParcelRequest {
 
 	
+	private String description;
 	
+	private ParcelCategory category;
+	
+	private String photoUrl;
+
+	private Boolean fragile;
+
+	@DecimalMin(value = "0.01")
+	private BigDecimal weight;
+
+	@DecimalMin(value = "0.0", inclusive = false)
+	private BigDecimal value;
+	
+	private Boolean restrictedItemsDeclared;
 	
 	
 }
